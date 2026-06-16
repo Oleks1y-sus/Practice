1a325c59c0225d0f1555be4a71a4da6bd39573cd
Oleksii Duda <dudaschool2@gmail.com>
2026-06-16 21:58:12 +0300

    downloaded extra npm files and trying to fix autoscroll

---
+++ script/slider.js
@@ -0,0 +1,22 @@
+const splide = new Splide('#testimonials-splide', {
+  type: 'loop',
+  perPage: 2,
+  perMove: 1,
+  gap: '1rem',
+  autoScroll: {
+    speed: 0.8,
+    pauseOnHover: true,
+  },
+//   arrows: false,
+  pagination: false,
+  breakpoints: {
+    768: {
+      perPage: 1,
+    },
+  },
+});
+
+splide.mount({ AutoScroll: SplideAutoScroll });
+
+document.getElementById('testimonials-prev').addEventListener('click', () => splide.go('<'));
+document.getElementById('testimonials-next').addEventListener('click', () => splide.go('>'));
